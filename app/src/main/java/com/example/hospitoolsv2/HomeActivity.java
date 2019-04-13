56d1182ba3b74139dc9fb7ab5ab33c936bb20f11
Winston Parris <anthonyparris16@gmail.com>
2019-04-13 13:25:23 -0500

    combined Android Manifest Files from Main activity and Map actvitiy, also created intent for between main activity and find hospitals button

--- app/src/main/java/com/example/hospitoolsv2/HomeActivity.java
+++ app/src/main/java/com/example/hospitoolsv2/HomeActivity.java
@@ -6,12 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.esri.arcgisruntime.findplaces.MainActivity;
+
 public class HomeActivity extends AppCompatActivity {
 
     public static DatabaseManager databaseManager;
 
 Button create_Patient_BTN;
 Button view_patient_BTN;
+Button find_hospitals_BTN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +25,23 @@ Button view_patient_BTN;
 
         create_Patient_BTN = findViewById(R.id.create_btn);
         view_patient_BTN = findViewById(R.id.view_btn);
+        find_hospitals_BTN = findViewById(R.id.find_Hospital);
+
+
 
         create_Patient_BTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent create_Patient = new Intent(HomeActivity.this, CreatePatient.class);
                 HomeActivity.this.startActivity(create_Patient);
+            }
+        });
+
+        find_hospitals_BTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent find_hospitals = new Intent(HomeActivity.this, MainActivity.class);
+                HomeActivity.this.startActivity(find_hospitals);
             }
         });
 
